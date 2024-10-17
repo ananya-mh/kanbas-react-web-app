33ba9c6848a666c9c1f65eec10b0b60bbceb4cf7
Your Name <you@example.com>
2024-10-17 19:02:55 -0400

    Assignment 3

--- src/Kanbas/Courses/Assignments/index.tsx
+++ src/Kanbas/Courses/Assignments/index.tsx
@@ -1,10 +1,14 @@
 import { BsGripVertical } from "react-icons/bs";
-import LessonControlButtons from "../Modules/LessonControlButtons";
 import AssignmentsControls from "./AssignmentsControls";
 import { HiOutlinePencilAlt } from "react-icons/hi";
 import AssignmentControlButtons from "./AssignmentControlButtons";
+import { Link, useParams} from "react-router-dom";
+import * as db from "../../Database";
+import ControlButtons from "./ControlButtons";
 
 export default function Assignments() {
+  const { cid } = useParams();
+  const assignments = db.assignments;
   return (
     <div>
       <AssignmentsControls />
@@ -12,184 +16,51 @@ export default function Assignments() {
       <br />
       <br />
       <br />
-      <ul id="wd-modules" className="list-group rounded-0">
-        <li className="wd-module list-group-item p-0 mb-5 fs-5 border-gray ">
+      <ul id="wd-assignments" className="list-group rounded-0">
+        <li className="wd-assignments list-group-item p-0 mb-5 fs-5 border-gray ">
           <div className="dropdown-toggle wd-title p-3 ps-2 bg-secondary ">
-            {" "}
             <BsGripVertical className="me-2 fs-3 " />
-            <AssignmentControlButtons />
             Assignments
+            <ControlButtons />
           </div>
-          <ul className="wd-lessons list-group rounded-0">
-            <li className="wd-lesson list-group-item p-3 ps-1">
-              <div className="row assignment-item py-3 align-items-center">
-                <div className="col-md-1 d-flex align-items-center">
-                  <BsGripVertical className="me-2 fs-3" />
-                  <HiOutlinePencilAlt className="me-2 fs-3 " color="green" />
-                </div>
-                <div className="col-md-9">
-                  <b>
-                    <a
-                      className="wd-assignment-link text-decoration-none text-muted"
-                      href="#/Kanbas/Courses/1234/Assignments/123"
-                    >
-                      A1 
-                    </a>
-                  </b>
-                  <div className="module-info text-muted">
-                    <span className="text-danger">Multiple Modules</span> |
-                    <span>
-                      {" "}
-                      <b>Not available until May 6 at 12:00am</b>
-                    </span>{" "}
-                    |<br />
-                    <span>
-                      <b>Due</b> May 13 at 11:59pm
-                    </span>{" "}
-                    |<span> 100 pts</span>
+          <ul className="wd-assignment list-group rounded-0">
+            {assignments
+              .filter((assignment: any) => assignment.course === cid)
+              .map((assignment: any) => (
+                <li className="wd-assignment list-group-item p-3 ps-1">
+                  <div className="row assignment-item py-3 align-items-center">
+                    <div className="col-md-1 d-flex align-items-center">
+                      <BsGripVertical className="me-2 fs-3" />
+                      <HiOutlinePencilAlt className="me-2 fs-3 " color="green" />
+                    </div>
+                    <div className="col-md-9">
+                      <b>
+                        <Link
+                          className="wd-assignment-link text-decoration-none text-muted"
+                          to={`/Kanbas/Courses/${cid}/Assignments/${assignment._id}/`}
+                        >
+                          {assignment.title}
+                        </Link>
+                      </b>
+                      <div className="module-info text-muted">
+                        <span className="text-danger">Multiple Modules</span> |
+                        <span>
+                          {" "}
+                          <b>Not available until May 6 at 12:00am</b>
+                        </span>{" "}
+                        |<br />
+                        <span>
+                          <b>Due</b> May 13 at 11:59pm
+                        </span>{" "}
+                        |<span> 100 pts</span>
+                      </div>
+                    </div>
+                    <div className="col-md-2 text-end">
+                      <AssignmentControlButtons />
+                    </div>
                   </div>
-                </div>
-                <div className="col-md-2 text-end">
-                  <LessonControlButtons />
-                </div>
-              </div>
-            </li>
-
-            <li className="wd-lesson list-group-item p-3 ps-1">
-              <div className="row assignment-item py-3 align-items-center">
-                <div className="col-md-1 d-flex align-items-center">
-                  <BsGripVertical className="me-2 fs-3" />
-                  <HiOutlinePencilAlt className="me-2 fs-3 " color="green" />
-                </div>
-                <div className="col-md-9">
-                  <b>
-                    <a
-                      className="wd-assignment-link text-decoration-none text-muted"
-                      href="#/Kanbas/Courses/1234/Assignments/123"
-                    >
-                      A2 
-                    </a>
-                  </b>
-                  <div className="module-info text-muted">
-                    <span className="text-danger">Multiple Modules</span> |
-                    <span>
-                      {" "}
-                      <b>Not available until May 6 at 12:00am</b>
-                    </span>{" "}
-                    |<br />
-                    <span>
-                      <b>Due</b> May 13 at 11:59pm
-                    </span>{" "}
-                    |<span> 100 pts</span>
-                  </div>
-                </div>
-                <div className="col-md-2 text-end">
-                  <LessonControlButtons />
-                </div>
-              </div>
-            </li>
-
-            <li className="wd-lesson list-group-item p-3 ps-1">
-              <div className="row assignment-item py-3 align-items-center">
-                <div className="col-md-1 d-flex align-items-center">
-                  <BsGripVertical className="me-2 fs-3" />
-                  <HiOutlinePencilAlt className="me-2 fs-3 " color="green" />
-                </div>
-                <div className="col-md-9">
-                  <b>
-                    <a
-                      className="wd-assignment-link text-decoration-none text-muted"
-                      href="#/Kanbas/Courses/1234/Assignments/123"
-                    >
-                      A3 
-                    </a>
-                  </b>
-                  <div className="module-info text-muted">
-                    <span className="text-danger">Multiple Modules</span> |
-                    <span>
-                      {" "}
-                      <b>Not available until May 6 at 12:00am</b>
-                    </span>{" "}
-                    |<br />
-                    <span>
-                      <b>Due</b> May 13 at 11:59pm
-                    </span>{" "}
-                    |<span> 100 pts</span>
-                  </div>
-                </div>
-                <div className="col-md-2 text-end">
-                  <LessonControlButtons />
-                </div>
-              </div>
-            </li>
-            <li className="wd-lesson list-group-item p-3 ps-1">
-              {" "}
-              <div className="row assignment-item py-3 align-items-center">
-                <div className="col-md-1 d-flex align-items-center">
-                  <BsGripVertical className="me-2 fs-3" />
-                  <HiOutlinePencilAlt className="me-2 fs-3 " color="green" />
-                </div>
-                <div className="col-md-9">
-                  <b>
-                    <a
-                      className="wd-assignment-link text-decoration-none text-muted"
-                      href="#/Kanbas/Courses/1234/Assignments/123"
-                    >
-                      A4 
-                    </a>
-                  </b>
-                  <div className="module-info text-muted">
-                    <span className="text-danger">Multiple Modules</span> |
-                    <span>
-                      {" "}
-                      <b>Not available until May 6 at 12:00am</b>
-                    </span>{" "}
-                    |<br />
-                    <span>
-                      <b>Due</b> May 13 at 11:59pm
-                    </span>{" "}
-                    |<span> 100 pts</span>
-                  </div>
-                </div>
-                <div className="col-md-2 text-end">
-                  <LessonControlButtons />
-                </div>
-              </div>
-            </li>
-            <li className="wd-lesson list-group-item p-3 ps-1">
-              {" "}
-              <div className="row assignment-item py-3 align-items-center">
-                <div className="col-md-1 d-flex align-items-center">
-                  <BsGripVertical className="me-2 fs-3" />
-                  <HiOutlinePencilAlt className="me-2 fs-3 " color="green" />
-                </div>
-                <div className="col-md-9">
-                  <b>
-                    <a
-                      className="wd-assignment-link text-decoration-none text-muted"
-                      href="#/Kanbas/Courses/1234/Assignments/123"
-                    >
-                      A5 
-                    </a>
-                  </b>
-                  <div className="module-info text-muted">
-                    <span className="text-danger">Multiple Modules</span> |
-                    <span>
-                      {" "}
-                      <b>Not available until May 6 at 12:00am</b>
-                    </span>{" "}
-                    |<br />
-                    <span>
-                      <b>Due</b> May 13 at 11:59pm
-                    </span>{" "}
-                    |<span> 100 pts</span>
-                  </div>
-                </div>
-                <div className="col-md-2 text-end">
-                  <LessonControlButtons />
-                </div>
-              </div>
-            </li>
+                </li>
+              ))}
           </ul>
         </li>
       </ul>{" "}
